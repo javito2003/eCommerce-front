dfaaace9f50ce170db68ffc232f650fae004790b
root <root@ip-172-31-19-28.sa-east-1.compute.internal>
2022-09-05 19:48:00 +0000

    comm

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import config from "../config"
 
 const Login = () => {
     const loginFn = async() => {
-        window.open(`${config.API.URL}/api/auth/google`, "_self")
+        window.open(`http://ec2-177-71-251-4.sa-east-1.compute.amazonaws.com:3001/api/auth/google`, "_self")
     }
 
   return (
